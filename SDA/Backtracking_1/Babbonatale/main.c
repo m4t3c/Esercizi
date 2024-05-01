0ef30c30f1d373adb333f21ee744484ac9e33c69
Matteo Coppa <matteo.coppa18@gmail.com>
2024-05-01 13:47:56 +0200

    Sistemati gli es di Backtracking

--- SDA/Backtracking_1/Babbonatale/main.c
+++ SDA/Backtracking_1/Babbonatale/main.c
@@ -5,7 +5,7 @@
 extern void BabboNatale(const int* pacchi, size_t pacchi_size, int p);
 
 int main(void) {
-	int pacchi[5] = {10, 11, 3, 1, 1};
+	int pacchi[5] = {10, 11, 1, 3, 3};
 	size_t pacchi_size = 5;
 	int p = 20;
 	BabboNatale(pacchi, pacchi_size, p);
